aa69bc32b26d79414945602b950f80ad054ec1b9
Alket Rexhepi <alketii@gmail.com>
2018-10-23 18:09:54 +0200

    some incomplte things just to save them

--- game.gd
+++ game.gd
@@ -52,6 +52,9 @@ var tileset_load = []
 var faction
 var techtree
 
+var units = {}
+var cells = []
+
 func _ready():
 	randomize()
 	
@@ -62,6 +65,7 @@ func _ready():
 	map_size = Vector2(map['width'],map['height'])
 	map_area = map_size * Vector2(tile_size,tile_size)
 	
+	printt(map["heightFactor"],map_size)
 	
 	# Setup camera
 
@@ -84,6 +88,9 @@ func _ready():
 	$water.get_mesh().set_size(map_area)
 	$terrain.get_mesh().set_size(map_area)
 	
+	$terrain.get_mesh().set_subdivide_width(map_size.x)
+	#$terrain.get_mesh().set_subdivide_depth(map_size.y)
+	
 	var splatmap = ImageTexture.new()
 	splatmap.load("res://data/maps/"+global.map_name+"/splatmap.png")
 	
@@ -103,7 +110,7 @@ func _ready():
 	water.load("res://data/tilesets/"+global.tileset_name+"/textures/water.png")
 	
 	$water.get_surface_material(0).set_shader_param("texturemap",water)
-	$water.set_translation(Vector3(0,map["waterLevel"]*0.14,0))
+	$water.set_translation(Vector3(0,map["waterLevel"]*0.40,0))
 	
 	$terrain.get_mesh().get_material().set_shader_param("source",heightmap)
 	$terrain.get_mesh().get_material().set_shader_param("splatmap",splatmap)
@@ -116,10 +123,11 @@ func _ready():
 	
 	techtree = GSC.read_json("res://data/techtrees/"+global.techtree_name+"/"+global.techtree_name+".json")
 	faction = GSC.read_json("res://data/techtrees/"+global.techtree_name+"/factions/"+global.faction_name+"/"+global.faction_name+".json")
+	faction = faction["faction"]
 
 	
 	i = 0
-	for res in faction["faction"]["starting-resources"]["resource"]:
+	for res in faction["starting-resources"]["resource"]:
 		var res_new = res_p.instance()
 		var res_tex = ImageTexture.new()
 		res_tex.load("res://data/techtrees/"+global.techtree_name+"/resources/"+res["name"]+"/images/"+res["name"]+".bmp")
@@ -142,8 +150,14 @@ func _ready():
 	for y in range(map_size.y):
 		for x in range(map_size.x):
 			o = map['resourceMap'][i]
+			s = int(map['heightMap'][i][0])
+			
+			if o == 0 or s > 3:
+				cells.append(0)
+			else:
+				cells.append(1)
+			
 			if o > 0 and o != 10:
-				s = int(map['heightMap'][i][0])
 				var obj
 				var rand
 				if o <= 10:
@@ -173,37 +187,55 @@ func _ready():
 			
 			i += 1
 	
-	# Setup initial units TMP
+	# Initial objects
+	
+	for unit_load in GSC.get_dir_list("res://data/techtrees/"+global.techtree_name+"/factions/"+global.faction_name+"/units"):
+		# TODO open json and get all daes
+		units[unit_load] = load("res://data/techtrees/"+global.techtree_name+"/factions/"+global.faction_name+"/units/"+unit_load+"/models/"+unit_load+".dae")
+	
 	var player_no = 1
 	for player in map['startLocations']:
-		var obj = pyramid.instance()
-		var x = int(player[0])
-		var y = int(player[1])
-		s = int(map['heightMap'][y*map_size.y+x][0])
-		z = s*0.20
-		var unit_new = unit.instance()
-		unit_new.translate(Vector3(x*tile_size-map_area.x/2,z,y*tile_size-map_area.y/2))
-		unit_new.set_scale(Vector3(obj_scale,obj_scale,obj_scale))
-		unit_new.set_rotation_degrees(Vector3(0,randi()%359,0))
-		unit_new.unit_name = "pyramid"
-		unit_new.add_child(obj)
-		unit_new.player_id = player_no
+		for unit_load in faction["starting-units"]["unit"]:
+			for amount in range(0,unit_load["amount"]):
+				var x = player[0]
+				var y = player[1]
+				var obj = units[unit_load["name"]].instance()
+				var unit_name = unit_load["name"]
+				place_obj(x,y,obj,unit_name,player_no)
 		player_no += 1
-		$units.add_child(unit_new)
-		
-		for m in obj.get_children()[0].get_children():
-			for ss in range(m.get_mesh().get_surface_count()):
-				var mat = m.get_mesh().surface_get_material(ss)
-				if mat != null:
-					#mat.flags_transparent = true
-					mat.flags_unshaded = true
-					#mat.params_use_alpha_scissor = true
-					#mat.params_specular_mode = true
-					m.get_mesh().surface_set_material(ss,mat)
-		add_child(obj)
+
+func place_obj(x,y,obj,obj_name,player_no):
+	var unit_new = unit.instance()
+	unit_new.translate(near_free_cell(x,y))
+	unit_new.set_scale(Vector3(obj_scale,obj_scale,obj_scale))
+	unit_new.set_rotation_degrees(Vector3(0,randi()%359,0))
+	unit_new.unit_name = obj_name
+	unit_new.add_child(obj)
+	unit_new.player_id = player_no
+	$units.add_child(unit_new)
+	for m in obj.get_children()[0].get_children():
+		for ss in range(m.get_mesh().get_surface_count()):
+			var mat = m.get_mesh().surface_get_material(ss)
+			if mat != null:
+				mat.flags_unshaded = true
+				#mat.params_use_alpha_scissor = true
+				m.get_mesh().surface_set_material(ss,mat)
+	add_child(unit_new)
+
+func near_free_cell(x,y):
+	# TMP
+	var cell_pos = cells[y*map_size.y+x]
+	
+	while cell_pos == 1:
+		x += 2
+		if cells[y*map_size.y+x] == 0:
+			cell_pos = cells[y*map_size.y+x]
+	
+	cells[y*map_size.y+x] = 1
+	var z = int(map['heightMap'][y*map_size.y+x][0])*0.35
+	return Vector3(x*tile_size-map_area.x/2,z,y*tile_size-map_area.y/2)
 
 func _on_main_menu_button_down():
-	print("A")
 	get_tree().change_scene("res://main_menu.tscn")
 
 
